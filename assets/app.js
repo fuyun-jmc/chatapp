f916e2682f92997296e79d21ab3aff2711981c55
浮云 <Jmc20120506@outlook.com>
2026-08-08 16:43:01 +0800

    fix: 发送消息后输入框不清空 (v189)

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@
  * ============================================================ */
 (function () {
   'use strict';
-  console.log('[chatapp] app.js build v195 loaded');
+  console.log('[chatapp] app.js build v196 loaded');
 
   var CFG = window.CHAT_CONFIG || {};
   var PHONE_RE = /^1[3-9]\d{9}$/;
